d392706a4a548c5b35515625bfc7d25ef8af5aba
BMosby <BMosby>
2017-05-10 13:18:49 -0600

    adding testing files, updated algo

--- Fundamental_Frequency.py
+++ Fundamental_Frequency.py
@@ -17,13 +17,6 @@ class Signal():
         """
         #end goal is to include a variable for HNR =True/False then we can do it all in one function and then get_HNR would call this function passing in HNR=True
         
-        #first plot
-        #plotting signal
-        sig_len = len( self.signal )
-        domain = np.linspace( 0, sig_len * time_step, sig_len )
-        plt.subplot(221)
-        plt.plot( domain, self.signal, label = 'signal' )
-        
         total_time = time_step * len( self.signal )
         Nyquist_Frequency = 1. / ( time_step * 2 )#if we decide to use sample rate then we would change this to nf=sample_rate/2
         
@@ -41,8 +34,7 @@ class Signal():
         signal = np.fft.ifft( fft_signal )
         
         global_peak = max( abs( signal ) )
-        #plotting the global peak with the signal
-        plt.plot( domain, np.ones( sig_len ) * global_peak )
+
         
         
         #note->if HNR: then put HNR window len in list with regular window len and do same for both(iterate through list) so code duplication is reduced
@@ -54,11 +46,7 @@ class Signal():
         frame_len = window_len / time_step
         num_frames = max( 1, int( len( signal ) / frame_len + .5 ) ) #there has to be at least one frame
         segmented_signal = [ signal[ int( i * frame_len ) : int( ( i+1 ) * frame_len ) ] for i in range( num_frames + 1 ) ]
-        
-        #plotting the segments on the signal
-        for i in range( num_frames ):
-            plt.plot( domain[ int( i*frame_len ) ] * np.ones( 50 ), np.linspace( -2, 2, 50 ) )
-        plt.legend()
+
     
         #intializing variables before for loop so they are not localized 
         cands_for_all_frames = [ ]
@@ -88,8 +76,13 @@ class Signal():
             sincM = np.tile( u, ( len( s ), 1 ) ) - np.tile( s[ :, np.newaxis ], ( 1, len( u ) ) )
             y = np.dot( x, np.sinc( sincM / T ) )
             return y
-                
-        #this calculates maxima of autocorrelation for each segment.
+        def gauss_window( t, T ):
+            if t < -.5 * T or t > 1.5 * T:
+                return 0
+            else:
+                return ( np.e**( -12 * ( t / T -.5 )**2 ) - np.e**( -12 ) ) / ( 1 - np.e**( -12 ) )
+            
+        #this for loop calculates maxima of autocorrelation for each segment.
         for index in range( len( segmented_signal ) ):
             #this is to ignore the occasional empty segments (when the signal can be segmented into equal parts it leaves 1 empty array at the end)
             if len( segmented_signal[ index ] ) == 0:
@@ -106,19 +99,11 @@ class Signal():
                 segment = segmented_signal[ index ]
                 r_x = estimated_autocorrelation( segment )
                 
-                plt.subplot( 222 )
-                plt.plot( np.linspace( time_begin, time_end, len( r_x ) ), r_x, label = 'r_x' )
-                plt.legend()
-                
                 time_array = np.linspace( time_begin, time_end, len( r_x ) ) 
                 
                 #do we want to multiply by hanning window to window the interpolation (as done in paper)?
-                vals = np.nan_to_num( sinc_interp( r_x, time_array, np.linspace( time_begin, time_end, len( r_x ) * 8 ) ) ) #* np.hanning( len( r_x ) * 2 ) 
+                vals = np.nan_to_num( sinc_interp( r_x, time_array, np.linspace( time_begin, time_end, len( r_x ) *8 ) ) )  
                 time_array = np.linspace( time_begin, time_end, len( vals ) )
-                
-                plt.subplot( 223 )#??
-                plt.plot( time_array, vals, label = 'r_x interpolated' )
-                plt.legend()
                 
                 if len( vals.nonzero()[ 0 ] ) != 0:          
                     #pu.indexes returns the indexes of peaks above the normalized threshold of .5
@@ -127,22 +112,13 @@ class Signal():
                     max_place_possible = 1. / min_pitch
                     min_place_possible = 1. / max_pitch
                     
-                    #plot all maxima places on plot above
-                    plt.plot( maxima_places, maxima_values, 'o' )
-                    
                     #deleting maxima that don't yield a frequency between min and max pitch
                     top_vals_elim = maxima_places[ maxima_places < max_place_possible ]
                     corrs_maxima_vals = maxima_values[ maxima_places < max_place_possible ]
                     maxima_places = top_vals_elim[ top_vals_elim > min_place_possible ]
                     maxima_values = corrs_maxima_vals[ top_vals_elim > min_place_possible ]
                     
-                    #plotting maxima after invalid values have been eliminated
-                    plt.subplot( 224 )#??
-                    plt.plot( time_array, vals, label = 'r_x windowed & interpolated, new bounds for max' )
-                    plt.legend()
-                    plt.plot( maxima_places, maxima_values, 'o' )
-                    
-                    #unvoiced candidate, which is always present.          
+                    #creating a list of (index, strengths) where index indicates the index in maxima_places that corresponds to the strength          
                     strengths = [ val - octave_cost * np.log2( min_pitch * place ) for place, val in zip( maxima_places, maxima_values ) ]
                     indexes_with_vals = [ ( idx, i ) for idx, i in enumerate( strengths ) ]
                     highest_vals = sorted( indexes_with_vals, reverse = True , key = lambda i:i[ 1 ] )
@@ -152,6 +128,8 @@ class Signal():
                         cands_for_all_frames.append( len( highest_vals ) + 1 )
                     else:
                         cands_for_all_frames.append( max_num_candidates )
+                      
+                    #creating a list of candidates (including the unvoiced candidate)    
                     candidate_strength = [ ( maxima_places[ i[ 0 ] ], i[ 1 ] ) for i in highest_vals[ :max_num_candidates-1 ] ]
                     unvoiced_strength = voicing_threshold + max( 0, 2 - ( ( local_peak ) / float( global_peak ) ) / ( ( silence_threshold ) / ( 1. + voicing_threshold ) ) )
                     candidate_strength.append( [ 0, unvoiced_strength ] ) #unvoiced candidate and strength are added to dictionary
@@ -162,8 +140,7 @@ class Signal():
                     candidate_strength.append( [ 0, unvoiced_strength ] )
                     
                 cands_for_all_frames.append( candidate_strength )
-                
-        plt.show()
+        
         #finding total number of paths possible based off of number of candidates
         total_paths = 1
         for x in cands_for_all_frames[ 0 : : 2 ]:
@@ -189,20 +166,26 @@ class Signal():
         def transition_cost( path ):
             """Finds transition cost for a specific path, returns a value"""
             sum_total = 0
+            
             for x in range( len( path ) - 1 ):
                 if path[ x ] == 0 and path[ x+1 ] == 0:
                     pass
+                
                 elif path[ x ] == 0 or path[ x + 1 ]==0:
                     sum_total += voiced_unvoiced_cost
+                    
                 elif path[ x ] != 0 and  path[ x + 1 ] != 0:
                     sum_total += octave_jump_cost * abs( np.log2( path[ x ] / path[ x + 1 ] ) )
+                    
             return sum_total
         
         def sum_strengths( path ):
             """ sums all the strengths of a path of different frequencies, returns resulting value."""
             sum_total = 0
+            
             for x in range( len( path ) ):
                 sum_total += cands_for_all_frames[ 1 : : 2 ][ x ][ path[ x ] ][ 1 ]
+                
             return sum_total
         
         total_cost = [ transition_cost( path ) - sum_strengths( path ) for path in all_paths ]
